00566a50c01fa42ce43a2a9adeb4dfba3d348f6d
Joselyne61 <nyirabagwizajose@gmail.com>
2025-09-25 11:24:36 +0200

    Add files via upload

---
+++ EXERCISE 3/project97 about stack.py
@@ -0,0 +1,17 @@
+stack=[]
+stack.append("step1")
+stack.append("step2")
+stack.append("step3")
+
+stack.pop()
+stack.pop()
+print(stack)
+
+ur=[]
+ur.append("exercise A")
+ur.append("exercise B")
+ur.append("exercise C")
+
+ur.pop()
+print(ur)
+
